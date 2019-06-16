90b1a52c932742983bc0c0e056ca23c975a343a0
jhyfugug <1838660395@qq.com>
2019-06-16 20:58:29 +0800

    Day 6 - 编写配置文件

---
+++ www/config_default.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+__author__ = 'jhyfugug'
+
+configs = {
+    'db':{
+        'host':'127.0.0.1',
+        'port':'3306',
+        'use':'www-data',
+        'password':'www-data',
+        'database':'awesome'
+    },
+    'session':{
+        'secret':'QwEsOmE'
+    }
+}
+
+
+
---
+++ www/config_override.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+__author__ = 'jhyfugug'
+
+configs = {
+    'db':{
+        'hsot':'127.0.0.1'
+    }
+}
+
